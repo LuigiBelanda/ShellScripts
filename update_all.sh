f817b8cd40b2d0ed992ec4e5a24d80ff39c33385
LuigiBelanda <luigi.belanda.milani@gmail.com>
2022-06-07 16:36:38 -0300

    Create update_node.sh / Add update node in update_all.sh

--- update_all.sh
+++ update_all.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
-# ----------------------------- UPDATE (snaps, flatpaks e pacotes deb) ----------------------------- #
+# ----------------------------- UPDATE (snaps, flatpaks e pacotes deb, etc) ----------------------------- #
+
+# dbpkgs
 sudo apt update -y 
 sudo apt dist-upgrade -y
 sudo apt upgrade -y 
 sudo apt full-upgrade -y 
+
+# node
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stalbe
+
+# snap and flatpak
 flatpak update -y 
 snap refresh
+
+# remove 
 sudo apt autoclean
 sudo apt autoremove -y 
---
+++ update_node.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo ""
+echo "========================================="
+echo "UPDATE NODE (STABLE)"
+echo "========================================="
+echo ""
+
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stalbe
