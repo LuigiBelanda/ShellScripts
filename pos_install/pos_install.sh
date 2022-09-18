e2a74726a69f9479df0142de39b24981fc61abdb
LuigiBelanda <luigi.belanda.milani@gmail.com>
2022-09-18 13:31:07 -0300

    Add If exists folders / files - Change color echo - More comments

--- pos_install/pos_install.sh
+++ pos_install/pos_install.sh
@@ -1,111 +1,154 @@
 #!/usr/bin/env bash
 
+
+
 # Author: Luigi Belanda
 # OS: Pop!_Os 22.04 LTS
 # Terminal: Fish + Starship
 
-dir=$HOME/Downloads/ShellScripts/pos_install
 
+
+# =================== COLORS ===================
+# https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+
+# Reset
+Color_Off='\033[0m' # Text Reset
+
+# Regular Colors
+Red='\033[0;31m'    # Red
+Green='\033[0;32m'  # Green
+
+# Bold High Intensity
+BIRed='\033[1;91m'    # Red
+BIGreen='\033[1;92m'  # Green
+
+
+
+# =================== SET VAR DIR ===================
+set dir $HOME/Downloads/ShellScripts/pos_install/
+echo -e "${BIRed}VAR DIR = $HOME/Downloads/ShellScripts/pos_install/${BIRed}"
+echo
+
+
+
+
+# =================== VERIFY REPOS AND FILES EXISTS ===================
+if [[ -d "$HOME/Programming/ShellScripts/pos_install" ]]; then
+    if [[ -e "$HOME/Programming/ShellScripts/pos_install/list_prog_snap.txt" ]]; then
+        echo -e "${Color_Off}$HOME/Programming/ShellScripts/pos_install/list_prog_snap.txt${Color_Off} ${BIGreen}FILE EXIST!${BIGreen}"
+        if [[ -e "$HOME/Programming/ShellScripts/pos_install/list_prog_flat.txt" ]]; then
+            echo -e "${Color_Off}$HOME/Programming/ShellScripts/pos_install/list_prog_flat.txt${Color_Off} ${BIGreen}FILE EXIST!${BIGreen}"
+            if [[ -e "$HOME/Programming/ShellScripts/pos_install/list_prog_deb.txt" ]]; then
+                echo -e "${Color_Off}$HOME/Programming/ShellScripts/pos_install/list_prog_deb.txt${Color_Off} ${BIGreen}FILE EXIST!${BIGreen}"
+                if [[ -e "$HOME/Programming/ShellScripts/pos_install/list_prog_apt.txt" ]]; then
+                    echo -e "${Color_Off}$HOME/Programming/ShellScripts/pos_install/list_prog_apt.txt${Color_Off} ${BIGreen}FILE EXIST!${BIGreen}"
+                    set dir $HOME/Programming/ShellScripts/pos_install/
+                    echo
+                    echo -e "${BIGreen}NEW VAR DIR = $HOME/Programming/ShellScripts/pos_install/${BIGreen}"
+                fi
+            fi
+        fi
+    fi
+fi
+
+
+
+# =================== FUNCTIONS ===================
 init() {
-    echo 
-    echo "Selecione uma opção!"
+    echo
+    echo -e "${Color_Off}Selecione uma opção! De preferência execute na seguinte os passos seguintes!${Color_Off}"
     echo
     echo "1 - Instalar programas (instalar snaps, flatpaks, deb etc)"
-    echo "2 - Criar pastas pessoais e mover arquivos (SOMENTE APÓS TERMINAR A INSTALAÇÃO)"
+    echo "2 - Criar pastas pessoais e mover arquivos (SOMENTE APÓS TERMINAR A INSTALAÇÃO DO PASSO 1)"
     echo "3 - Instalar Fish Shell e Starship (irá sair do script no final!)"
     echo "4 - Install NodeJS + NPM + Yarn"
     echo "5 - Config Git (SOMENTE DEPOIS DO PASSO 1)"
     echo "6 - Sair do Script"
     echo
     echo "Verifique se o arquivo ~/.config/fish/config.fish tem a linha (starship init fish | source)"
-    echo 
+    echo
     echo "Qual opção você escolhe?"
 
-    while :
-    do
+    while :; do
         read opcao_selecionada
         case $opcao_selecionada in
-             
-            1) install_apts;;
-            2) move_dir;;
-            3) install_fish;;
-            4) install_node_npm_yarn;;
-            5) config_git;;
-            6) exit
+
+        1) install_apts ;;
+        2) move_dir ;;
+        3) install_fish ;;
+        4) install_node_npm_yarn ;;
+        5) config_git ;;
+        6) exit ;;
 
         esac
     done
 }
 
 rem_locks() {
-    # Removendo travas eventuais do apt #
-    sudo rm /var/lib/dpkg/lock-frontend ; sudo rm /var/cache/apt/archives/lock ; sudo rm /var/lib/dpkg/lock ;
+    # rm locks apt
+    sudo rm /var/lib/dpkg/lock-frontend
+    sudo rm /var/cache/apt/archives/lock
+    sudo rm /var/lib/dpkg/lock
 
-    # Adicionando/Confirmando arquitetura de 32 bits #
+    # Add/Confirm 32 bits
     sudo dpkg --add-architecture i386
 
-    # Atualizando o repositório #
+    # update
     sudo apt update -y
 }
 
 install_apts() {
     rem_locks
 
-    # lendo arquivo com apps apt
+    # read lines -> list_prog_apt.txt
     while read line; do
         sudo apt install $line -y
-    done < "$dir/list_prog_apt.txt"
-
-    # terminando de instalar NPM - NODE etc
-    sudo npm cache clean -f
-    sudo npm install -g n
-    sudo n stable
-    sudo npm install --global yarn
+    done <"$dir/list_prog_apt.txt"
 
     # flat repo
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
     # refresh flat and snap
-    flatpak update -y 
+    flatpak update -y
     snap refresh
 
-    # proximo passo
+    # next
     install_snap_and_flat
 }
 
 install_snap_and_flat() {
-    # lendo arquivo com apps snap
+    # read lines -> list_prog_snap.txt
     while read line; do
         sudo snap install $line
-    done < "$dir/list_prog_snap.txt"
+    done <"$dir/list_prog_snap.txt"
 
-    # lendo arquivo com apps flatpak
+    # read lines -> list_prog_flat.txt
     while read line; do
         sudo flatpak install -y flathub $line
-    done < "$dir/list_prog_snap.txt"  
+    done <"$dir/list_prog_flat.txt"
 
-    # proximo passo
+    # next
     install_deb
 }
 
 install_deb() {
     # Code
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
     sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
     sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
     rm -f packages.microsoft.gpg
 
     # OneDrive
     echo 'deb http://download.opensuse.org/repositories/home:/jstaf/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:jstaf.list
-    curl -fsSL https://download.opensuse.org/repositories/home:jstaf/xUbuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_jstaf.gpg > /dev/null
+    curl -fsSL https://download.opensuse.org/repositories/home:jstaf/xUbuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_jstaf.gpg >/dev/null
 
     # Insomnia
-    echo "deb [trusted=yes arch=amd64] https://download.konghq.com/insomnia-ubuntu/ default all" \
-        | sudo tee -a /etc/apt/sources.list.d/insomnia.list
+    echo "deb [trusted=yes arch=amd64] https://download.konghq.com/insomnia-ubuntu/ default all" |
+        sudo tee -a /etc/apt/sources.list.d/insomnia.list
 
     # Brave
     sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
     # Beekeeper Studio
     wget --quiet -O - https://deb.beekeeperstudio.io/beekeeper.key | sudo apt-key add -
@@ -113,32 +156,32 @@ install_deb() {
 
     sudo apt update -y
     sudo apt dist-upgrade -y
-    sudo apt upgrade -y 
-    sudo apt full-upgrade -y 
+    sudo apt upgrade -y
+    sudo apt full-upgrade -y
 
-    # lendo arquivo com apps deb
+    # read lines -> list_prog_deb.txt
     while read line; do
         sudo apt install $line -y
-    done < "$dir/list_prog_snap.txt"  
+    done <"$dir/list_prog_deb.txt"
 
     sudo apt update -y
     sudo apt dist-upgrade -y
-    sudo apt upgrade -y 
-    sudo apt full-upgrade -y 
-    sudo apt autoclean -y 
-    sudo apt autoremove -y 
+    sudo apt upgrade -y
+    sudo apt full-upgrade -y
+    sudo apt autoclean -y
+    sudo apt autoremove -y
 
     init
 }
 
 move_dir() {
-    # Criando pastas de organização pessoal
+    # create folders
     mkdir $HOME/Programming -m 777
     mkdir $HOME/OneDriver
     mkdir -p $HOME/OneDriver/FATEC
     mkdir -p $HOME/OneDriver/Pessoal
 
-    # Movendo os scripts para a nova pasta Programming para melhor organização
+    # mv scripts
     mv $HOME/Downloads/ShellScripts $HOME/Programming/
 
     init
@@ -152,15 +195,23 @@ install_fish() {
     chsh -s /usr/bin/fish
     mkdir -p ~/.config/fish
 
-    # Starship (baixando - criando arquivo de config - colocando preset)
+    # Fish alias
+    alias update_all $HOME/Programming/ShellScripts/update_all.sh
+    chmod +x $HOME/Programming/ShellScripts/update_all.sh
+
+    # Fisher and plugins
+    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+    fisher install jorgebucaran/autopair.fish
+
+    # Starship (download)
     curl -sS https://starship.rs/install.sh | sh
 
-    # add na linha 3 do arquivo config.fish o conteúdo (starship init fish | source)
-    sed -i "3i starship init fish | source" ~/.config/fish/config.fish
+    # create files (config and add preset)
+    mkdir -p ~/.config && touch ~/.config/starship.toml
+    starship preset bracketed-segments >~/.config/starship.toml
 
-    # criando arquivos de config e add preset
-    mkdir -p ~/.config && touch ~/.config/starship.toml 
-    starship preset bracketed-segments > ~/.config/starship.toml
+    # add in line 3 -> arquivo config.fish -> content (starship init fish | source)
+    sed -i "3i starship init fish | source" ~/.config/fish/config.fish
 
     exit
 }
@@ -168,18 +219,23 @@ install_fish() {
 install_node_npm_yarn() {
     rem_locks
 
+    # install npm and corepack enable
     sudo apt install npm -y
     sudo npm install npm -g
     sudo corepack enable
 
+    # install n
     sudo npm cache clean --force
     sudo npm install n -g
 
-    # https://github.com/tj/n 
+    # install node and npm latest version stable
+    # https://github.com/tj/n
     sudo n stable
 
+    # install yarn
     sudo npm install yarn -g
 
+    # versions
     echo
     echo "Versões (Node - NPM - Yarn)"
     node -v
@@ -189,8 +245,9 @@ install_node_npm_yarn() {
     init
 }
 
-config_git () {
-    echo 
+config_git() {
+    # get data
+    echo
     echo "CONFIG DO GIT"
     echo "Username Git:"
     read userGit
@@ -198,9 +255,11 @@ config_git () {
     echo "Email Git:"
     read emailGit
 
+    # add data in git config
     git config --global user.name $userGit
     git config --global user.email $emailGit
 
+    # show data
     echo
     echo "Dados Registrados"
     git config user.name
@@ -210,5 +269,6 @@ config_git () {
 }
 
 
-# ------------------------ Init func ------------------------ #
+
+# =================== INIT FUNC / PROGRAM ===================
 init
