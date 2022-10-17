e78af35407b68d1d962c9050eaaab2726434ee47
LuigiBelanda <luigi.belanda.milani@gmail.com>
2022-10-17 09:42:44 -0300

    Remove apps / New Paths

--- pos_install/pos_install.sh
+++ pos_install/pos_install.sh
@@ -2,10 +2,10 @@
 
 # Author: Luigi Belanda
 # OS: Pop!_Os 22.04 LTS
-# Terminal: Fish + Starship + FiraCode Nerd Font
+# Terminal: Fish + Starship + FiraCode Nerd Font + Dracula
 
 # OS themes:  
-# Cursor (Sweet-cursors - https://www.gnome-look.org/p/1393084)
+# Cursor 
 # Apps (Sweet-Dark-v40 - https://www.gnome-look.org/p/1253385/)
 # shell (Sweet-Dark-v40 - https://www.gnome-look.org/p/1253385/)
 # icons (default)
@@ -28,26 +28,19 @@ Green='\033[0;32m' # Green
 BIRed='\033[1;91m'   # Red
 BIGreen='\033[1;92m' # Green
 
-set dir $HOME/Downloads/ShellScripts/
-echo -e "${BIRed}VAR DIR = $HOME/Downloads/ShellScripts/${BIRed} ${Color_Off}${Color_Off}"
-
-if [[ -d "$HOME/Programming/ShellScripts/" ]]; then
-    if [[ -d "$HOME/Programming/ShellScripts/pos_install" ]]; then
-        if [[ -e "$HOME/Programming/ShellScripts/pos_install/list_prog_snap.txt" ]]; then
-            if [[ -e "$HOME/Programming/ShellScripts/pos_install/list_prog_flat.txt" ]]; then
-                if [[ -e "$HOME/Programming/ShellScripts/pos_install/list_prog_deb.txt" ]]; then
-                    if [[ -e "$HOME/Programming/ShellScripts/pos_install/list_prog_apt.txt" ]]; then
+if [[ -d "home/luigibelanda/Programming/ShellScripts/" ]]; then
+    if [[ -d "home/luigibelanda/Programming/ShellScripts/pos_install" ]]; then
+        if [[ -e "home/luigibelanda/Programming/ShellScripts/pos_install/list_prog_snap.txt" ]]; then
+            if [[ -e "home/luigibelanda/Programming/ShellScripts/pos_install/list_prog_flat.txt" ]]; then
+                if [[ -e "home/luigibelanda/Programming/ShellScripts/pos_install/list_prog_deb.txt" ]]; then
+                    if [[ -e "home/luigibelanda/Programming/ShellScripts/pos_install/list_prog_apt.txt" ]]; then
                         echo
-                        echo -e "${Color_Off}$HOME/Programming/ShellScripts/${Color_Off} ${BIGreen}FOLDER EXIST!${BIGreen}"
-                        echo -e "${Color_Off}$HOME/Programming/ShellScripts/pos_install${Color_Off} ${BIGreen}FOLDER EXIST!${BIGreen}"
-                        echo -e "${Color_Off}$HOME/Programming/ShellScripts/pos_install/list_prog_snap.txt${Color_Off} ${BIGreen}FILE EXIST!${BIGreen}"
-                        echo -e "${Color_Off}$HOME/Programming/ShellScripts/pos_install/list_prog_flat.txt${Color_Off} ${BIGreen}FILE EXIST!${BIGreen}"
-                        echo -e "${Color_Off}$HOME/Programming/ShellScripts/pos_install/list_prog_deb.txt${Color_Off} ${BIGreen}FILE EXIST!${BIGreen}"
-                        echo -e "${Color_Off}$HOME/Programming/ShellScripts/pos_install/list_prog_apt.txt${Color_Off} ${BIGreen}FILE EXIST!${BIGreen}"
-
-                        set dir $HOME/Programming/ShellScripts/
-                        echo
-                        echo -e "${BIGreen}NEW VAR DIR = $HOME/Programming/ShellScripts/${BIGreen} ${Color_Off}${Color_Off}"
+                        echo -e "${Color_Off}home/luigibelanda/Programming/ShellScripts/${Color_Off} ${BIGreen}FOLDER EXIST!${BIGreen}"
+                        echo -e "${Color_Off}home/luigibelanda/Programming/ShellScripts/pos_install${Color_Off} ${BIGreen}FOLDER EXIST!${BIGreen}"
+                        echo -e "${Color_Off}home/luigibelanda/Programming/ShellScripts/pos_install/list_prog_snap.txt${Color_Off} ${BIGreen}FILE EXIST!${BIGreen}"
+                        echo -e "${Color_Off}home/luigibelanda/Programming/ShellScripts/pos_install/list_prog_flat.txt${Color_Off} ${BIGreen}FILE EXIST!${BIGreen}"
+                        echo -e "${Color_Off}home/luigibelanda/Programming/ShellScripts/pos_install/list_prog_deb.txt${Color_Off} ${BIGreen}FILE EXIST!${BIGreen}"
+                        echo -e "${Color_Off}home/luigibelanda/Programming/ShellScripts/pos_install/list_prog_apt.txt${Color_Off} ${BIGreen}FILE EXIST!${BIGreen}"
                     else
                         echo -e "${BIRed}FILE LIST_PROG_APT.TXT DOES NOT EXISTS${BIRed} ${Color_Off}${Color_Off}"
                     fi
@@ -61,26 +54,19 @@ if [[ -d "$HOME/Programming/ShellScripts/" ]]; then
             echo -e "${BIRed}FILE LIST_PROG_SNAP.TXT DOES NOT EXISTS${BIRed} ${Color_Off}${Color_Off}"
         fi
     else
-        echo -e "${BIRed}$HOME/Programming/ShellScripts/pos_install - PATH INCORRECT OR FOLDERS NOT CREATED AND FILES NOT MOVED${BIRed} ${Color_Off}${Color_Off}"
+        echo -e "${BIRed}home/luigibelanda/Programming/ShellScripts/pos_install - PATH INCORRECT OR FOLDERS NOT CREATED AND FILES NOT MOVED${BIRed} ${Color_Off}${Color_Off}"
     fi
 else
-    echo -e "${BIGreen}CREATING FOLDER $HOME/Programming and mkdir $HOME/OneDriver (OneDriver/Pessoal - OneDriver/FATEC)${BIGreen} ${Color_Off}${Color_Off}"
+    echo -e "${BIGreen}CREATING FOLDER home/luigibelanda/Programming and mkdir home/luigibelanda/OneDriver (OneDriver/Pessoal - OneDriver/FATEC)${BIGreen} ${Color_Off}${Color_Off}"
 
     # create folders
-    mkdir $HOME/Programming -m 777
-    mkdir $HOME/OneDriver
-    mkdir -p $HOME/OneDriver/FATEC
-    mkdir -p $HOME/OneDriver/Pessoal
-
-    echo -e "${BIGreen}MOVE FOLDER $HOME/Downloads/ShellScripts for $HOME/Programming/ShellScripts${BIGreen} ${Color_Off}${Color_Off}"
-    echo -e "${BIGreen}RECOMENDADED: RESTART TERMINAL ${BIGreen} ${Color_Off}${Color_Off}"
+    mkdir home/luigibelanda/Programming -m 777
+    mkdir home/luigibelanda/OneDriver
+    mkdir -p home/luigibelanda/OneDriver/FATEC
+    mkdir -p home/luigibelanda/OneDriver/Pessoal
 
     # mv scripts
-    mv $HOME/Downloads/ShellScripts $HOME/Programming/
-
-    set dir $HOME/Programming/ShellScripts/
-    echo
-    echo -e "${BIGreen}NEW VAR DIR = $HOME/Programming/ShellScripts/${BIGreen} ${Color_Off}${Color_Off}"
+    mv home/luigibelanda/Downloads/ShellScripts home/luigibelanda/Programming/
 fi
 
 # =================== FUNCTIONS ===================
@@ -95,7 +81,7 @@ init() {
     echo "5 - Sair do Script"
     echo
     echo "Verifique se o arquivo ~/.config/fish/config.fish tem a linha (starship init fish | source)"
-    echo "Verifique se os paths estão certos ($HOME/Programming/ShellScripts)"
+    echo "Verifique se os paths estão certos (home/luigibelanda/Programming/ShellScripts)"
     echo
     echo "Qual opção você escolhe?"
 
@@ -133,7 +119,7 @@ install_apts() {
     # read lines -> list_prog_apt.txt
     while read line; do
         sudo apt install $line -y
-    done <"$dir/pos_install/list_prog_apt.txt"
+    done < "/home/luigibelanda/Programming/ShellScripts/pos_install/list_prog_apt.txt"
 
     # flat repo
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -150,12 +136,12 @@ install_snap_and_flat() {
     # read lines -> list_prog_snap.txt
     while read line; do
         sudo snap install $line
-    done <"$dir/pos_install/list_prog_snap.txt"
+    done < "/home/luigibelanda/Programming/ShellScripts/pos_install/list_prog_snap.txt"
 
     # read lines -> list_prog_flat.txt
     while read line; do
         sudo flatpak install -y flathub $line
-    done <"$dir/pos_install/list_prog_flat.txt"
+    done < "/home/luigibelanda/Programming/ShellScripts/pos_install/list_prog_flat.txt"
 
     # next
     install_deb
@@ -163,7 +149,7 @@ install_snap_and_flat() {
 
 install_deb() {
     # Code
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
     sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
     sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
     rm -f packages.microsoft.gpg
@@ -176,10 +162,6 @@ install_deb() {
     sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
     echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
-    # Beekeeper Studio
-    wget --quiet -O - https://deb.beekeeperstudio.io/beekeeper.key | sudo apt-key add -
-    echo "deb https://deb.beekeeperstudio.io stable main" | sudo tee /etc/apt/sources.list.d/beekeeper-studio-app.list
-
     sudo apt update -y
     sudo apt dist-upgrade -y
     sudo apt upgrade -y
@@ -188,7 +170,7 @@ install_deb() {
     # read lines -> list_prog_deb.txt
     while read line; do
         sudo apt install $line -y
-    done <"$dir/pos_install/list_prog_deb.txt"
+    done < "/home/luigibelanda/Programming/ShellScripts/pos_install/list_prog_deb.txt"
 
     sudo apt update -y
     sudo apt dist-upgrade -y
@@ -204,7 +186,7 @@ install_fish() {
     # functions
     rem_locks
 
-    if [[ -d "$HOME/Programming/ShellScripts/" ]]; then
+    if [[ -d "home/luigibelanda/Programming/ShellScripts/" ]]; then
         # Fish
         sudo apt install fish
         chsh -s /usr/bin/fish
@@ -219,25 +201,27 @@ install_fish() {
 
         # create files (config and add preset)
         mkdir -p ~/.config && touch ~/.config/starship.toml
-        starship preset bracketed-segments >~/.config/starship.toml
+        starship preset bracketed-segments > ~/.config/starship.toml
 
         # add in line 3 -> arquivo config.fish -> content (starship init fish | source)
         sed -i "3i starship init fish | source" ~/.config/fish/config.fish
 
         # Fish alias
-        if [[ -e "$HOME/Programming/ShellScripts/update_all.sh" ]]; then
-            alias update_all $HOME/Programming/ShellScripts/update_all.sh
-            chmod +x $HOME/Programming/ShellScripts/update_all.sh
+        if [[ -e "home/luigibelanda/Programming/ShellScripts/update_all.sh" ]]; then
+            alias update_all home/luigibelanda/Programming/ShellScripts/update_all.sh
+            chmod +x home/luigibelanda/Programming/ShellScripts/update_all.sh
         fi
 
         # set default shell
         which fish
         echo /usr/local/bin/fish | sudo tee -a /etc/shells
         chsh -s /usr/local/bin/fish
+        
+        fisher install dracula/fish
 
         exit
     else
-        echo -e "${BIRed}ERROR! $HOME/Programming/ShellScripts/update_all.sh NOT EXISTS OR PATH INCORRECT${BIRed} ${Color_Off}${Color_Off}"
+        echo -e "${BIRed}ERROR! home/luigibelanda/Programming/ShellScripts/update_all.sh NOT EXISTS OR PATH INCORRECT${BIRed} ${Color_Off}${Color_Off}"
         init
     fi
 
@@ -273,9 +257,9 @@ install_node_npm_yarn() {
 }
 
 config_git() {
-    if [[ -d "$HOME/Programming/ShellScripts/" ]]; then
-        if [[ -d "$HOME/Programming/ShellScripts/dotfiles" ]]; then
-            if [[ -e "$HOME/Programming/ShellScripts/dotfiles/.gitconfig" ]]; then
+    if [[ -d "home/luigibelanda/Programming/ShellScripts/" ]]; then
+        if [[ -d "home/luigibelanda/Programming/ShellScripts/dotfiles" ]]; then
+            if [[ -e "home/luigibelanda/Programming/ShellScripts/dotfiles/.gitconfig" ]]; then
                 #remove ~/.gitconfig file 
                 rm ~/.gitconfig
 
@@ -286,15 +270,15 @@ config_git() {
                 echo -e "${BIGreen}Create a simbolic link (~/Programming/ShellScripts/dotfiles/.gitconfig ----> ~/.gitconfig)${BIGreen} ${Color_Off}${Color_Off}"
             else
                 echo
-                echo -e "${BIRed}$HOME/Programming/ShellScripts/dotfiles/.gitconfig - FILE DOES NOT EXISTS OR PATH IS INCORRECT${BIRed} ${Color_Off}${Color_Off}"
+                echo -e "${BIRed}home/luigibelanda/Programming/ShellScripts/dotfiles/.gitconfig - FILE DOES NOT EXISTS OR PATH IS INCORRECT${BIRed} ${Color_Off}${Color_Off}"
             fi
         else
             echo
-            echo -e "${BIRed}$HOME/Programming/ShellScripts/dotfiles - DOTFILES FOLDER DOES NOT EXISTS OR PATH IS INCORRECT${BIRed} ${Color_Off}${Color_Off}"
+            echo -e "${BIRed}home/luigibelanda/Programming/ShellScripts/dotfiles - DOTFILES FOLDER DOES NOT EXISTS OR PATH IS INCORRECT${BIRed} ${Color_Off}${Color_Off}"
         fi
     else
         echo
-        echo -e "${BIRed}$HOME/Programming/ShellScripts/ - PATH IS INCORRECT OR FOLDER NOT EXISTS${BIRed} ${Color_Off}${Color_Off}"
+        echo -e "${BIRed}home/luigibelanda/Programming/ShellScripts/ - PATH IS INCORRECT OR FOLDER NOT EXISTS${BIRed} ${Color_Off}${Color_Off}"
     fi
 
     init
